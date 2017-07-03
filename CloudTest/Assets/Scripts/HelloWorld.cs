ff3e7b9b41ffc715674eebfbd73434a41b3f80c0
Alex S <s_alexander@hotmail.de>
2017-07-03 13:48:33 +0200

    Added Scripts with Hello World

---
+++ CloudTest/Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HelloWorld : MonoBehaviour {
+    public Text textField;
+    private bool textChanged = false;
+    private string savedText;
+
+    private void Start()
+    {
+        savedText = textField.text;
+    }
+
+    public void PushAndToggleText() {
+        if (!textChanged)
+        {
+            textField.text = "Hello World";
+        }
+        else {
+            textField.text = savedText;
+        }
+        textChanged = !textChanged;
+    }
+}
